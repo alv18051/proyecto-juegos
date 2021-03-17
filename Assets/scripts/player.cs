e11e3b8ab45a9bdff242ee2aa28ef3012a78b396
Alejandro Ortega <ort18248@uvg.edu.gt>
2021-03-17 15:54:34 -0600

    Arreglado el FPSController y el Raycast del disparo
    El FPS Controller ya no es un cohete y el Raycast del disparo se genera hacia el centro de la pantalla en lugar de a un lado de ella.

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    private gun armaActual;
+    [SerializeField] public gun armaActual;
     private bool shoot;
-    private bool estaDisparando;
+    private bool estaDisparando = false;
     private bool EstaRecargando;
     private RaycastHit hit;
     public int balas;
     public int maxBalas = 100;
 
+    [SerializeField] Camera cam = null;
+
+    Color debugRayColor = Color.green;
+    Color debugShootRayColor = Color.blue;
 
     // Start is called before the first frame update
     void Start()
     {
-
         balas = maxBalas;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Debug.DrawRay(cam.ScreenToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f)), cam.transform.forward * 10f, debugRayColor); // Solo para verificar el raycast
+        if (Input.GetMouseButton(0))
         {
-            shoot = true;
-
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            shoot = false;
-        }
-
-        if (shoot && Input.GetMouseButtonDown(0))
-        {
+            debugRayColor = Color.red; // Cambiando el color del rayo debug, para verificar si presiono click
+            
             Shoot();
+        }
+        else // Este else solo es para proposito de prueba, para cambiar el rayo de regreso a verde
+        {
+            debugRayColor = Color.green;
         }
     }
 
     void Shoot()
     {
+        
         if (estaDisparando || armaActual == null)
         {
             return;
@@ -50,21 +50,23 @@ public class player : MonoBehaviour
         {
             shoot = false;
         }
-        Ray ray = Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f));
+        estaDisparando = true;
+        // Rayo azul que se dispara en la direccion del disparo, permanece en pantalla segun la cadencia del arma
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * armaActual.alcance, debugShootRayColor, armaActual.cadencia);
 
-        ray.origin = armaActual.GunBarrel.position; 
-
-        if (Physics.Raycast(ray, out hit, armaActual.alcance))
+        // Disparando el rayo desde el centro de la pantalla en lugar del barril del arma, al hacerlo desde el barril se modifica la direccion
+        // del rayo y no pega en el centro de la pantalla
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, armaActual.alcance))
         {
             if (hit.collider.CompareTag("zombie"))
             {
                 Destroy(hit.collider.gameObject);
             }
         }
-        estaDisparando = true;
-        StartCoroutine(CoolDown());
+        // Rayo para visualizar hacia donde apuntaria el disparo al salir del barril del arma
+        Debug.DrawRay(armaActual.GunBarrel.position, (hit.point - armaActual.GunBarrel.position), Color.yellow, armaActual.cadencia);
 
-        
+        StartCoroutine(CoolDown());
     }
 
     IEnumerator CoolDown()
